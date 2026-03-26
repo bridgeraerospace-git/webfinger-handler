1676cca5a0c9f66ffa35b272cac6e31146711ccf
Bridger Aerospace <s.bennett@bridgeraerospace.com>
2026-03-26 15:27:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export default {
     const MY_EMAIL = "s.bennett@bridgerfmsaero.com"; // Your Tailscale admin email
     
     // The Entra ID OIDC Issuer URL format
-    const ISSUER_URL = `https://login.microsoftonline.us{TENANT_ID}/v2.0`;
+    const ISSUER_URL = `https://login.microsoftonline.us/16663179-f23a-4d36-85e2-5f061f488171/v2.0`;
     // ------------------------------
 
     if (resource === `acct:${MY_EMAIL}`) {
